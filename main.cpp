b2f464f264e4b7b4fff6d3132e838602a26d26a7
Yi Tseng <a86487817@gmail.com>
2018-05-14 01:06:19 +0800

    Add options and frontend

--- main.cpp
+++ main.cpp
@@ -1,5 +1,59 @@
+#include <fstream>
 #include <iostream>
 
+#include "ir/ir.h"
+
+#include "frontends/common/options.h"
+#include "frontends/common/parseInput.h"
+#include "frontends/p4/frontend.h"
+
+#include "lib/nullstream.h"
+
+class FTTOptions : public CompilerOptions {
+ public:
+    cstring outputFile = nullptr;
+    FTTOptions() {
+      registerOption("-o", "outfile",
+              [this](const char* arg) { this->outputFile = arg; return true; },
+              "Write output to outfile");
+     }
+};
+
+using FTTContext = P4CContextWithOptions<FTTOptions>;
+
 int main(int argc, char *const argv[]) {
-    std::cout << "Hello world" << std::endl;
+    AutoCompileContext autoFTTContext(new FTTContext);
+    auto& options = FTTContext::get().options();
+    options.langVersion = CompilerOptions::FrontendVersion::P4_16;
+    options.compilerVersion = "0.0.1";
+
+    if (options.process(argc, argv) != nullptr)
+        options.setInputFile();
+    if (::errorCount() > 0)
+        return 1;
+
+    auto program = P4::parseP4File(options);
+
+    if (program == nullptr || ::errorCount() > 0) {
+        std::cerr << "Can't parse P4 file " << options.file << std::endl;
+        return 1;
+    }
+
+    try {
+        P4::FrontEnd fe;
+        program = fe.run(options, program);
+    } catch (const Util::P4CExceptionBase &bug) {
+        std::cerr << bug.what() << std::endl;
+        return 1;
+    }
+
+    if (program == nullptr || ::errorCount() > 0) {
+        std::cerr << "Can't process P4 file " << options.file << " by frontend"<< std::endl;
+        return 1;
+    }
+
+    if (options.dumpJsonFile) {
+        JSONGenerator(*openFile(options.dumpJsonFile, true), true) << program << std::endl;
+    }
+
 }
